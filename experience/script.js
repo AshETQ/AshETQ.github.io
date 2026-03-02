49be459b3d4d6863690729d690ce1a312984fd8b
AshETQ <115114973+AshETQ@users.noreply.github.com>
2026-03-02 11:37:13 -0500

    Update script.js
    Updated name, added comments to fix photos

--- experience/script.js
+++ experience/script.js
@@ -31,15 +31,15 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+//var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+//(function(){
+//var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+//s1.async=true;
+//s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+//s1.charset='UTF-8';
+//s1.setAttribute('crossorigin','*');
+//s0.parentNode.insertBefore(s1,s0);
+//})();
 // End of Tawk.to Live Chat
 
 
@@ -65,11 +65,12 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
-        $("#favicon").attr("href","/assets/images/favicon.png");
+        document.title = "Experience | Portfolio Ash Chase";
+        $("#favicon").attr("href","/assets/images/favicon.png"); // Add the image you want to be at the top (thinking ICRA at the entrance photo? Circular looks decent?)
     }
     else {
-        document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","/assets/images/favhand.png");
+        document.title = "Ash's Portfolio";
+        $("#favicon").attr("href","/assets/images/favhand.png"); //edit to have an image of a star or something
     }
+
 });
